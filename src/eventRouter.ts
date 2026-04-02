6f1b6ffbcce539fd782a542a140ca7c072449f94
起司玉米蛋餅 <ericxpert@gmail.com>
2026-04-02 22:07:07 +0800

    chore: 根據事件類型回應不同的訊息，新增圖片回應

--- src/eventRouter.ts
+++ src/eventRouter.ts
@@ -13,19 +13,31 @@ export async function eventRouter(
     new Promise((resolve) => setTimeout(resolve, 50)), // 最多等 50ms
   ]);
 
-  await sendMessage(channelAccessToken, event.replyToken, [
-    {
-      type: "text",
-      text: "Meow!",
-    },
-    {
-      type: "text",
-      text: `Event type: ${event.message.type}`,
-    },
-    {
-      type: "image",
-      originalContentUrl: "https://s3.ccepr.dev/foo/IMG_2457.jpg",
-      previewImageUrl: "https://s3.ccepr.dev/foo/IMG_2457.jpg",
-    },
-  ]);
+  if (event.message.type === "text" && event.message.text === "🎲") {
+    await sendMessage(channelAccessToken, event.replyToken, [
+      {
+        type: "image",
+        originalContentUrl:
+          "https://s3.ccepr.dev/foo/2025-08-19%2021-35-17%20(2).jpg",
+        previewImageUrl:
+          "https://s3.ccepr.dev/foo/2025-08-19%2021-35-17%20(2).jpg",
+      },
+    ]);
+  } else {
+    await sendMessage(channelAccessToken, event.replyToken, [
+      {
+        type: "text",
+        text: "Meow!",
+      },
+      {
+        type: "text",
+        text: `Event type: ${event.message.type}`,
+      },
+      {
+        type: "image",
+        originalContentUrl: "https://s3.ccepr.dev/foo/IMG_2457.jpg",
+        previewImageUrl: "https://s3.ccepr.dev/foo/IMG_2457.jpg",
+      },
+    ]);
+  }
 }

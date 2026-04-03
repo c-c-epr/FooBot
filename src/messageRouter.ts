607ac273c35c7400d83de3c5c798a5fe78260df9
起司玉米蛋餅 <ericxpert@gmail.com>
2026-04-03 16:22:28 +0800

    refactor: 重構 messageRouter 以簡化事件處理邏輯

--- src/messageRouter.ts
+++ src/messageRouter.ts
@@ -6,36 +6,36 @@ export default async function messageRouter(
   channelAccessToken: string,
   ctx: ExecutionContext,
 ) {
-  if (event.message.type === "text" && event.message.text === "🎲") {
-    const imageList = [
-      "https://s3.ccepr.dev/foo/2025-08-19%2021-35-17%20(2).jpg",
-      "https://s3.ccepr.dev/foo/IMG_2457.jpg",
-    ];
+  switch (event.message.type) {
+    case "text":
+      switch (event.message.text) {
+        case "meow":
+          await sendMessage(channelAccessToken, event.replyToken, [
+            {
+              type: "text",
+              text: "Meow!",
+            },
+          ]);
+          break;
+        case "🎲":
+          const imageList = [
+            "https://s3.ccepr.dev/foo/2025-08-19%2021-35-17%20(2).jpg",
+            "https://s3.ccepr.dev/foo/IMG_2457.jpg",
+          ];
+          const diceRoll = Math.floor(Math.random() * imageList.length);
+          const selectedImage = imageList[diceRoll];
+          await sendMessage(channelAccessToken, event.replyToken, [
+            {
+              type: "image",
+              originalContentUrl: selectedImage,
+              previewImageUrl: selectedImage,
+            },
+          ]);
+        default:
+          console.warn(`Unhandled message text: ${event.message.text}`);
+      }
 
-    const diceRoll = Math.floor(Math.random() * imageList.length);
-
-    const selectedImage = imageList[diceRoll];
-
-    await sendMessage(channelAccessToken, event.replyToken, [
-      {
-        type: "image",
-        originalContentUrl: selectedImage,
-        previewImageUrl: selectedImage,
-      },
-    ]);
-  } else if (event.message.type === "text" && event.message.text === "meow") {
-    await sendMessage(channelAccessToken, event.replyToken, [
-      {
-        type: "text",
-        text: "Meow!",
-      },
-    ]);
-  } else {
-    await sendMessage(channelAccessToken, event.replyToken, [
-      {
-        type: "text",
-        text: `Event type: ${event.message.type}`,
-      },
-    ]);
+    default:
+      console.warn(`Unhandled message type: ${event.message.type}`);
   }
 }

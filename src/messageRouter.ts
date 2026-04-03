f4dc94d65827fd08d64869a1464a7416708fb4f3
起司玉米蛋餅 <ericxpert@gmail.com>
2026-04-03 20:40:24 +0800

    feat: 新增 CAPTCHA 事件處理以發送挑戰訊息

--- src/messageRouter.ts
+++ src/messageRouter.ts
@@ -32,6 +32,149 @@ export default async function messageRouter(
             },
           ]);
           break;
+        case "CAPTCHA":
+          await sendMessage(channelAccessToken, event.replyToken, [
+            {
+              type: "flex",
+              altText: "CAPTCHA Challenge",
+              contents: {
+                type: "bubble",
+                header: {
+                  type: "box",
+                  layout: "vertical",
+                  contents: [
+                    {
+                      type: "text",
+                      text: "請找出全部包含",
+                      size: "sm",
+                      color: "#ffffff",
+                      gravity: "center",
+                    },
+                    {
+                      type: "text",
+                      text: "小福",
+                      size: "3xl",
+                      color: "#ffffff",
+                      gravity: "center",
+                      weight: "bold",
+                    },
+                    {
+                      type: "text",
+                      text: "的圖片",
+                      size: "sm",
+                      color: "#ffffff",
+                      gravity: "center",
+                    },
+                  ],
+                  spacing: "none",
+                  margin: "none",
+                },
+                body: {
+                  type: "box",
+                  layout: "vertical",
+                  contents: [
+                    {
+                      type: "box",
+                      layout: "horizontal",
+                      contents: [
+                        {
+                          type: "image",
+                          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                          aspectRatio: "1:1",
+                          aspectMode: "cover",
+                          size: "full",
+                          backgroundColor: "#eaffea",
+                        },
+                        {
+                          type: "image",
+                          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                          aspectRatio: "1:1",
+                          size: "full",
+                          aspectMode: "cover",
+                        },
+                        {
+                          type: "image",
+                          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                          aspectRatio: "1:1",
+                          aspectMode: "cover",
+                          size: "full",
+                        },
+                      ],
+                      margin: "lg",
+                      spacing: "lg",
+                    },
+                    {
+                      type: "box",
+                      layout: "horizontal",
+                      contents: [
+                        {
+                          type: "image",
+                          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                          aspectRatio: "1:1",
+                          aspectMode: "cover",
+                          size: "full",
+                        },
+                        {
+                          type: "image",
+                          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                          aspectRatio: "1:1",
+                          size: "full",
+                          aspectMode: "cover",
+                        },
+                        {
+                          type: "image",
+                          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                          aspectRatio: "1:1",
+                          aspectMode: "cover",
+                          size: "full",
+                        },
+                      ],
+                      margin: "lg",
+                      spacing: "lg",
+                    },
+                    {
+                      type: "box",
+                      layout: "horizontal",
+                      contents: [
+                        {
+                          type: "image",
+                          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                          aspectRatio: "1:1",
+                          aspectMode: "cover",
+                          size: "full",
+                        },
+                        {
+                          type: "image",
+                          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                          aspectRatio: "1:1",
+                          size: "full",
+                          aspectMode: "cover",
+                        },
+                        {
+                          type: "image",
+                          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                          aspectRatio: "1:1",
+                          aspectMode: "cover",
+                          size: "full",
+                        },
+                      ],
+                      margin: "lg",
+                      spacing: "lg",
+                    },
+                  ],
+                },
+                styles: {
+                  header: {
+                    backgroundColor: "#70abff",
+                  },
+                  hero: {
+                    backgroundColor: "#70abff",
+                  },
+                },
+              },
+            },
+          ]);
+          break;
         default:
           console.warn(`Unhandled message text: ${event.message.text}`);
       }

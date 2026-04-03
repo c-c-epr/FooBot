c3ce3ef89eddb8f4e4ea77a76099424d62046564
起司玉米蛋餅 <ericxpert@gmail.com>
2026-04-03 20:47:03 +0800

    feat: 在 messageRouter 中新增 postback 行動的顯示文字

--- src/messageRouter.ts
+++ src/messageRouter.ts
@@ -88,6 +88,7 @@ export default async function messageRouter(
                             type: "postback",
                             label: "action",
                             data: "1",
+                            displayText: "A",
                           },
                         },
                         {
